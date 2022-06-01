0887bae7416391547bd9e1bd4f6969ec45cd0e76
= <=>
2022-06-01 15:44:28 -0500

    Message added

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -5,11 +5,13 @@ import React from 'react';
 const weightLetters = ['U','W','Z', 'N', 'T'];
 const skinnyLetters = ['I','O'];
 
-const Letter = ({ letter }) => {
+const Letter = ({ letter, id }) => {
     const upperLetter = letter.toUpperCase();
 
     return (
-        <div className={`${styles.letter} ${ weightLetters.find(el => el === upperLetter) ? 'more' : ''}`}>
+        <div className={`${styles.letter} ${ weightLetters.find(el => el === upperLetter) ? 'more' : ''}`} style={{
+            zIndex: `${id}`
+        }}>
             <span>{letter}</span> 
         </div>
     );
@@ -17,6 +19,8 @@ const Letter = ({ letter }) => {
 
 
 const Text = ({ word }) => {
+
+    const size = word.length;
 
     return <div className={styles.pacto}>
         <div className={styles.hastag}>
@@ -26,7 +30,7 @@ const Text = ({ word }) => {
             <div className={styles.paloma}>
                 <img src={Paloma} alt="Paloma" />    
             </div> 
-            {word.split('').map((letter, index) => <Letter key={index} letter={letter} />)}
+            {word.split('').map((letter, index) => <Letter key={index} letter={letter} id={size - index} />)}
         </div>
         <div className={styles.logo}>
             <img src={Logo} alt="Logo" />
